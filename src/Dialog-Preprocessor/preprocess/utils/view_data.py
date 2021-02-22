c28853e0059d7d3fe63d0e9f997465998328e32b
bangbangbang12315 <2016202218@ruc.edu.cn>
2021-02-22 10:55:01 +0800

    Delete src/Dialog-Preprocessor directory

--- src/Dialog-Preprocessor/preprocess/utils/view_data.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for idx, line in enumerate(sys.stdin):
-    line = line.strip().split('\t')
-    print("Line %d: %s" % (idx, line[0]))
-    for c in line[1:]:
-        print("\t%s" % c)
-    print('\n\n')
